ee93b73fc3d7a816e7525be57a3e2517aef7d682
Mav <riherr00@gmail.com>
2025-11-08 17:29:04 +0300

    Protected/public fix. Message game start

---
+++ src/seaBattle/protocol/messages/messagesRequest/MessageGameStart.java
@@ -0,0 +1,26 @@
+package seaBattle.protocol.messages.messagesRequest;
+
+import seaBattle.protocol.Protocol;
+import seaBattle.protocol.messages.MessageRequest;
+
+public class MessageGameStart extends MessageRequest {
+
+    private static final long serialVersionUID = 1L;
+
+    private String opponentNic;
+    private boolean yourTurn;
+
+    public MessageGameStart(byte id, String fromNic, long sessionId, String opponentNic, boolean yourTurn) {
+        super(Protocol.CMD_GAME_STARTS, fromNic, sessionId);
+        this.opponentNic = opponentNic;
+        this.yourTurn = yourTurn;
+    }
+
+    public String getOppNic() {
+        return opponentNic;
+    }
+
+    public boolean isYourTurn() {
+        return yourTurn;
+    }
+}
